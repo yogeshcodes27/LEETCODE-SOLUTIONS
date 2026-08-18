6be2c374b80c77a2222af401ac7e3699a46f8713
Yogesh S <yogeshsara70@gmail.com>
2026-08-18 19:26:53 +0530

    [1528] [Time Beats: 63.77%] [Memory Beats: 73.97%] - LeetPush

---
+++ JAVA/1528-shuffle-string/1528-Shuffle-String.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String restoreString(String s, int[] indices) {
+        char[] arr=new char[indices.length];
+        
+        for(int i=0;i<s.length();i++){
+           int in=indices[i];
+           arr[in]=s.charAt(i);
+        }
+        return new String(arr);
+    }
+}
