2b5e439f426ef553a623a9558c0d324a1ed26962
Yogesh S <yogeshsara70@gmail.com>
2026-08-18 19:48:04 +0530

    [1572] [Time Beats: 32.38%] [Memory Beats: 51.86%] - LeetPush

---
+++ JAVA/1572-matrix-diagonal-sum/1572-Matrix-Diagonal-Sum.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int diagonalSum(int[][] mat) {
+        int n=mat.length,m=mat[0]. length;
+        int sum=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if((i==j)||(i+j==n-1)){
+                    sum+=mat[i][j];
+                }
+            }
+        }
+        return sum;
+    }
+}
