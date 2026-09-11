3c880909822c1de98b55570f16f7e9787fb00668
Yogesh S <yogeshsara70@gmail.com>
2026-09-11 18:46:21 +0530

    [3483] [Time Beats: 93.87%] [Memory Beats: 87.60%] - LeetPush

---
+++ JAVA/DCP-09-26/3483-unique-3-digit-even-numbers/3483-Unique-3-Digit-Even-Numbers.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int totalNumbers(int[] digits) {
+        boolean[] used = new boolean[1000];
+        int count = 0;
+        for(int i = 0; i < digits.length; i++) {
+            if(digits[i] == 0)
+                continue;
+            for(int j = 0; j < digits.length; j++) {
+                if(j == i)
+                    continue;
+                for(int k = 0; k < digits.length; k++) {
+                    if(k == i || k == j)
+                        continue;
+                    if(digits[k] % 2 != 0)
+                        continue;
+                    int num = digits[i] * 100 + digits[j] * 10 + digits[k];
+                    if(!used[num]) {
+                        used[num] = true;
+                        count++;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+}
